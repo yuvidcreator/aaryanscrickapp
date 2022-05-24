8676aaf134a638ddb6250bf43bf8463ca6fb4b04
Yuvraj Limbole <yuvrajl.103@gmail.com>
2022-05-24 18:28:58 +0530

    Comming soon page created

---
+++ components/Layout.js
@@ -0,0 +1,15 @@
+import Header from '../components/Header'
+import Footer from './Footer';
+
+const Layout = ({ children }) => {
+    return (
+        <div>
+            <Header />
+            { children }
+
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Hero from '../components/Hero'
 
 export default function Home() {
   return (
@@ -10,7 +11,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className="text-center justify-center text-2xl font-bold mt-36">Welcome to Aryans Cricket Academy</h1>
+      {/* <h1 className="text-center justify-center text-2xl font-bold mt-36">Welcome to Aryans Cricket Academy</h1> */}
+
+      <Hero />
     </div>
   )
 }
